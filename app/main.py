e7b8b7d9b9b1621510edd319f602ac3c4e055014
gabrielbaute <gabrielbaute@gmail.com>
2026-01-17 13:29:13 -0400

    Reubicación del entrypoint. Oh, dramático giro de los acontecimientos, sigue dando el mismo resultado en la ubicación de los directorios

---
+++ app/main.py
@@ -0,0 +1,38 @@
+import uvicorn
+import logging
+import os
+
+from app.config import Config
+from app.api.app_factory import create_app
+from app.database.db_config import init_db
+
+Config.create_dirs()
+config = Config()
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(os.path.join(Config.LOGS_PATH, "app.log"), encoding="utf-8"),
+    ]    
+)
+
+app = create_app(config=config)
+init_db(instance_path=Config.INSTANCE_PATH)
+
+def run_server():
+    """
+    Run the FastAPI server.
+    """
+    uvicorn.run(
+        "app.main:app",
+        host=Config.API_HOST,
+        port=Config.API_PORT,
+        log_level="debug",
+        reload=True,
+    )
+
+if __name__ == "__main__":
+    run_server()
