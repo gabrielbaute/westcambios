49cee15d7cc3076dcbe2aee6a98ca33632a13f2a
gabrielbaute <gabrielbaute@gmail.com>
2026-01-17 15:14:47 -0400

    Estaba duplicando registros yo mismo en el seed. Qué bruto, pongale cero.

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,2 +1,6 @@
 from app.seeds.create_users import create_users
 from app.seeds.create_rates import create_rates
+
+if __name__ == "__main__":
+    create_users()
+    create_rates()
--- app/seeds/create_users.py
+++ app/seeds/create_users.py
@@ -7,19 +7,23 @@ from app.services import UserService
 
 def create_users():
     user_service = UserService()
-    user_in = UserCreate(
+    user_admin = UserCreate(
         email="admin@example.com",
         username="admin",
         password_hash="1234",
         is_active=True,
         role=UserRole.ADMIN
     )
-    user_in = UserCreate(
+    user_client = UserCreate(
         email="user@example.com",
         username="user",
         password_hash="1234",
         is_active=True,
         role=UserRole.CLIENT
     )
-    user_service.register_user(user_in)
-    user_service.register_user(user_in)
+    user_service.register_user(user_admin)
+    user_service.register_user(user_client)
+    user_service.dispose()
+
+if __name__ == "__main__":
+    create_users()
