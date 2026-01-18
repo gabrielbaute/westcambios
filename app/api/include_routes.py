070b0e746a49e718c40c00702fad754b3546f3e6
gabrielbaute <gabrielbaute@gmail.com>
2026-01-18 06:41:19 -0400

    Se agregó una ruta de health check para verificar el estado de la API

--- app/api/include_routes.py
+++ app/api/include_routes.py
@@ -4,6 +4,7 @@ Helper to load all the api routes.
 
 from fastapi import FastAPI
 
+from app.api.routes.health_routes import router as health_router
 from app.api.routes.rates_routes import router as rates_router
 from app.api.routes.admin_routes import router as admin_router
 from app.api.routes.user_routes import router as user_router
@@ -17,5 +18,6 @@ def include_routes(app: FastAPI, prefix: str):
     """
     app.include_router(auth_router, prefix=prefix, tags=["Authentication"])
     app.include_router(rates_router, prefix=prefix, tags=["Exchange Rates"])
-    app.include_router(admin_router, prefix=prefix, tags=["Administration"])
     app.include_router(user_router, prefix=prefix, tags=["Users"])
+    app.include_router(admin_router, prefix=prefix, tags=["Administration"])
+    app.include_router(health_router, prefix=prefix, tags=["Health"])
